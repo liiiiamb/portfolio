e0b84923c640f227ae02c6142adfe5e6ed07c6ed
Liam Boyd <notliamb@outlook.com>
2024-03-19 13:38:18 +0000

    Finished rest of content

--- script.js
+++ script.js
@@ -13,6 +13,50 @@ menuLinks.forEach(link => {
     });
 });
 
+gsap.registerPlugin(ScrollTrigger, ScrollTrigger);
+
+// REVEAL //
+gsap.utils.toArray("#animateUp").forEach(function (elem) {
+  ScrollTrigger.create({
+    trigger: elem,
+    start: "top 80%",
+    end: "bottom 20%",
+    markers: false,
+    onEnter: function () {
+      gsap.fromTo(
+        elem,
+        { y: 100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeave: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    },
+    onEnterBack: function () {
+      gsap.fromTo(
+        elem,
+        { y: -100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeaveBack: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    }
+  });
+});
+
 
 let cardIndex = 0;
 
